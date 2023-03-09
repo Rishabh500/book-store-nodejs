6a038a517d2c0ac0e61c730d06763cbb44cfcbf6
Rishabh jain <rishabh.j50@gmail.com>
2023-03-09 22:08:23 +0530

    first commit

--- routes/admin.js
+++ routes/admin.js
@@ -2,15 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const { getAddProduct, postAddProduct } = require('../controller/product');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product',getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product',postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
